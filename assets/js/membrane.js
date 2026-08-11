f6f19e144a6396f61ef76f91924974f47e173656
Zed James <drjames@thephoenixkingdom.com>
2026-08-11 15:09:28 -0500

    Preserve existing measure presence loader key

--- assets/js/membrane.js
+++ assets/js/membrane.js
@@ -168,7 +168,7 @@
     }
     loadScript("assets/js/home-vitality.js?v=1","data-home-vitality");
     if(document.getElementById("different")&&document.getElementById("idea")&&document.getElementById("science"))loadScript("assets/js/home-signature.js?v=1","data-home-signature");
-    if(document.getElementById("different"))loadScript("assets/js/home-measure-presence.js?v=1","data-home-measure-presence");
+    if(document.getElementById("different"))loadScript("assets/js/home-measure-presence.js?v=1","data-measure-presence");
     loadScript("assets/js/home-copy-polish.js?v=3","data-home-copy-polish");
   }
 
